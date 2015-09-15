d6088b63340a460927b3b6d41c831196208b7875
janstenpickle <jansen.chris@gmail.com>
2015-09-15 11:49:34 +0100

    Fix auto plugin scoping - fixes #1

--- build.sbt
+++ build.sbt
@@ -4,13 +4,11 @@ organization := "com.intenthq.sbt"
 
 name := "sbt-thrift-plugin"
 
-version := "1.0.0"
+version := "1.0.1"
 
 scalaVersion := "2.10.5"
 
 scalacOptions ++= Seq("-deprecation", "-feature")
-
-publishMavenStyle := false
 
 publishTo := {
   val nexus = "https://oss.sonatype.org/"
@@ -19,3 +17,24 @@ publishTo := {
   else
     Some("releases"  at nexus + "service/local/staging/deploy/maven2")
 }
+
+pomExtra := (
+  <url>https://github.com/intenthq/sbt-thrift-plugin</url>
+  <licenses>
+    <license>
+      <name>The MIT License (MIT)</name>
+      <url>https://github.com/intenthq/sbt-thrift-plugin/blob/master/LICENSE</url>
+      <distribution>repo</distribution>
+    </license>
+  </licenses>
+  <scm>
+    <url>git@github.com:intenthq/sbt-thrift-plugin.git</url>
+    <connection>scm:git:git@github.com:intenthq/sbt-thrift-plugin.git</connection>
+  </scm>
+  <developers>
+    <developer>
+      <id>intenthq</id>
+      <name>Intent HQ</name>
+    </developer>
+  </developers>
+)
--- src/main/scala/com/intenthq/sbt/ThriftPlugin.scala
+++ src/main/scala/com/intenthq/sbt/ThriftPlugin.scala
@@ -30,27 +30,27 @@ object ThriftPlugin extends AutoPlugin {
     val thriftSourceDir = SettingKey[File]("source-directory", "Source directory for thrift files. Defaults to src/main/thrift")
 
     val thriftJavaEnabled = SettingKey[Boolean]("java-enabled", "java generation is enabled. Default - yes")
-    val thriftGenerateJava = TaskKey[Seq[File]]("thrift-generate-java", "Generate java sources from thrift files")
+    val thriftGenerateJava = TaskKey[Seq[File]]("generate-java", "Generate java sources from thrift files")
     val thriftJavaOptions = SettingKey[Seq[String]]("thrift-java-options", "additional options for java thrift generation")
     val thriftOutputDir = SettingKey[File]("java-output-directory", "Directory where the java files should be placed. Defaults to sourceManaged")
 
     val thriftJsEnabled = SettingKey[Boolean]("js-enabled", "javascript generation is enabled. Default - no")
-    val thriftGenerateJs = TaskKey[Seq[File]]("thrift-generate-js", "Generate javascript sources from thrift files")
+    val thriftGenerateJs = TaskKey[Seq[File]]("generate-js", "Generate javascript sources from thrift files")
     val thriftJsOptions = SettingKey[Seq[String]]("thrift-js-options", "additional options for js thrift generation")
     val thriftJsOutputDir = SettingKey[File]("js-output-directory", "Directory where generated javascript files should be placed. default target/thrift-js")
 
     val thriftRubyEnabled = SettingKey[Boolean]("ruby-enabled", "ruby generation is enabled. Default - no")
-    val thriftGenerateRuby = TaskKey[Seq[File]]("thrift-generate-ruby", "Generate ruby sources from thrift files.")
+    val thriftGenerateRuby = TaskKey[Seq[File]]("generate-ruby", "Generate ruby sources from thrift files.")
     val thriftRubyOptions = SettingKey[Seq[String]]("thrift-ruby-options", "additional options for ruby thrift generation")
     val thriftRubyOutputDir = SettingKey[File]("ruby-output-directory", "Directory where generated ruby files should be placed. default target/thrift-ruby")
 
     val thriftPythonEnabled = SettingKey[Boolean]("python-enabled", "python generation is enabled. Default - no")
-    val thriftGeneratePython = TaskKey[Seq[File]]("thrift-generate-python", "Generate python sources from thrift files.")
+    val thriftGeneratePython = TaskKey[Seq[File]]("generate-python", "Generate python sources from thrift files.")
     val thriftPythonOptions = SettingKey[Seq[String]]("thrift-python-options", "additional options for python thrift generation")
     val thriftPythonOutputDir = SettingKey[File]("python-output-directory", "Directory where generated python files should be placed. default target/thrift-python")
 
     val thriftDelphiEnabled = SettingKey[Boolean]("delphi-enabled", "delphi generation is enabled. Default - no")
-    val thriftGenerateDelphi = TaskKey[Seq[File]]("thrift-generate-delphi", "Generate delphi sources from thrift files.")
+    val thriftGenerateDelphi = TaskKey[Seq[File]]("generate-delphi", "Generate delphi sources from thrift files.")
     val thriftDelphiOptions = SettingKey[Seq[String]]("thrift-delphi-options", "additional options for delphi thrift generation")
     val thriftDelphiOutputDir = SettingKey[File]("delphi-output-directory", "Directory where generated delphi files should be placed. default target/thrift-delphi")
   }
@@ -61,21 +61,20 @@ object ThriftPlugin extends AutoPlugin {
 
   override def trigger = allRequirements
 
-  val thriftConfig = config("thriftPlugin")
+  val Thrift = config("thrift")
 
-  val thriftSettings: Seq[Setting[_]] = Seq(
+  val thriftSettings: Seq[Setting[_]] = inConfig(Thrift)(Seq(
     thrift := "thrift",
-    thriftSourceDir <<= (sourceDirectory in Compile) { _ / "thrift" },
+    thriftSourceDir <<= sourceDirectory { _ / "main" / "thrift" },
     thriftJavaEnabled := true,
     thriftJavaOptions := Seq("hashcode"),
-    thriftOutputDir <<= (sourceManaged in Compile) { identity },
+    thriftOutputDir <<= sourceManaged  { identity },
     thriftGenerateJava <<= (thriftJavaEnabled, thriftSourceDir, thriftOutputDir, thrift, thriftJavaOptions, streams).map { (te, tsd, tod, t, to, s) =>
       if (te) {
         compileThrift(tsd, tod, t, "java", to, s.log, s.cacheDirectory / "thrift-java") }
       else
         Seq()
     },
-    sourceGenerators <+= thriftGenerateJava,
     thriftJsEnabled := false,
     thriftJsOptions := Seq(),
     thriftJsOutputDir := file("target/gen-js"),
@@ -85,7 +84,6 @@ object ThriftPlugin extends AutoPlugin {
       else
         Seq()
     },
-    sourceGenerators <+= thriftGenerateJs,
     thriftRubyEnabled := false,
     thriftRubyOptions := Seq(),
     thriftRubyOutputDir := file("target/gen-ruby"),
@@ -95,7 +93,6 @@ object ThriftPlugin extends AutoPlugin {
       else
         Seq()
     },
-    sourceGenerators <+= thriftGenerateRuby,
     thriftPythonEnabled := false,
     thriftPythonOptions := Seq(),
     thriftPythonOutputDir := file("target/gen-python"),
@@ -105,7 +102,6 @@ object ThriftPlugin extends AutoPlugin {
       else
         Seq()
     },
-    sourceGenerators <+= thriftGeneratePython,
     thriftDelphiEnabled := false,
     thriftDelphiOptions := Seq(),
     thriftDelphiOutputDir := file("target/gen-delphi"),
@@ -114,13 +110,17 @@ object ThriftPlugin extends AutoPlugin {
         compileThrift(tsd, tod, t, "delphi", to, s.log, s.cacheDirectory / "thrift-delphi") }
       else
         Seq()
-    },
-    sourceGenerators <+= thriftGenerateDelphi
+    }
+  )) ++ Seq[Setting[_]](
+    watchSources <++= thriftSourceDir.map { ( tdir ) => ( tdir ** "*" ).get },
+    sourceGenerators in Compile <+= thriftGenerateJava in Thrift,
+    sourceGenerators in Compile <+= thriftGenerateJs in Thrift,
+    sourceGenerators in Compile <+= thriftGenerateRuby in Thrift,
+    sourceGenerators in Compile <+= thriftGeneratePython in Thrift,
+    sourceGenerators in Compile <+= thriftGenerateDelphi in Thrift,
+    ivyConfigurations += Thrift
   )
 
-  override lazy val projectSettings =
-    Seq(ivyConfigurations += thriftConfig) ++
-    inConfig(Test)(thriftSettings) ++
-    inConfig(Compile)(thriftSettings)
+  override lazy val projectSettings = thriftSettings
 
 }
